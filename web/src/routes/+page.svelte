364520ff57f6a6f6899cbc0ee75a4b676e3350d2
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-10-07 02:26:38 -0700

    test

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -38,12 +38,12 @@
         >CULTURE　CLUB　友達　EXPLORE　楽しい　時間　アイスブレーカー
     </span>
 
-    <LeftDotGrid class="fixed top-10 left-10" />
+    <LeftDotGrid class="fixed top-10 left-10 lg:scale-100 scale-50" />
 
     <LeftLinesThingy class="fixed top-1/3 left-7" />
 
     <div class="flex w-screen fixed -top-1 left-20 justify-center">
-        <ScatteredSquares class="fixed" />
+        <ScatteredSquares class="fixed lg:scale-100 scale-50" />
     </div>
 
     <div
@@ -53,25 +53,23 @@
     </div>
 
     <div
-        class="fixed origin-center outer-spin -bottom-52 -left-56 md:-bottom-16 md:left-8 lg:-bottom-16 lg:left-8"
+        class="fixed origin-center outer-spin -bottom-52 -left-56 lg:-bottom-16 lg:left-8"
     >
-        <OuterCircleThingy class="scale-50 lg:scale-100 md:scale-100" />
+        <OuterCircleThingy class="scale-50 lg:scale-100" />
     </div>
     <div
-        class="fixed origin-center inner-spin bottom-4 left-1 lg:bottom-40 lg:left-64 md:bottom-40 md:left-64"
+        class="fixed origin-center inner-spin bottom-4 left-1 lg:bottom-40 lg:left-64"
     >
-        <MiddleCircleThingy class="scale-50 lg:scale-100 md:scale-100" />
+        <MiddleCircleThingy class="scale-50 lg:scale-100" />
     </div>
     <!-- <div
-        class="fixed bottom-4 left-1 lg:bottom-[20rem] lg:left-[22rem] md:bottom-[20rem] md:left-[22rem]"
+        class="fixed hidden bottom-40 left-[9.3rem] lg:bottom-[20rem] lg:left-[22rem] md:bottom-[20rem] md:left-[22rem]"
     >
         <iframe
-            width="150"
-            height="150"
             scrolling="no"
             frameborder="no"
             allow="autoplay"
-            class="rounded-lg"
+            class="rounded-lg w-16 h-16 lg:w-32 lg:h-32 md:w-32 md:h-32"
             title="Soundcloud"
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1735105293&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
         ></iframe>
@@ -83,13 +81,40 @@
         class="fixed lg:bottom-32 lg:right-32 md:bottom-32 md:right-32 right-6 bottom-16 lg:scale-125 md:scale-125 scale-75"
     />
     <Circles
-        class="fixed lg:top-32 lg:right-40 md:top-32 top-24 right-12 md:right-40 lg:scale-125 md:scale-125 scale-90"
+        class="fixed lg:top-32 lg:right-40 md:top-32 top-24 right-8 md:right-40 lg:scale-125 md:scale-125 scale-90"
     />
 
-    <div class="fixed top-2 left-40 font-rampartOne">
-        <span class="text-[10rem] text-theme-main relative">
-            日本文化<br />クラブ
-        </span>
+    <div
+        class="fixed lg:top-12 md:top-12 top-14 lg:left-40 md:left-40 left-12 font-rampartOne w-fit z-50"
+    >
+        <div
+            class="lg:text-[10rem] md:text-[10rem] text-8xl text-theme-main relative leading-tight heading-text"
+        >
+            <span
+                class="uppercase lg:text-2xl md:text-2xl text-lg absolute -rotate-[5deg] lg:left-6 md:left-6"
+                >japanese</span
+            >
+            <span
+                class="uppercase lg:text-2xl md:text-2xl text-lg absolute -rotate-[5deg] lg:left-1/2 md:left-1/2 lg:top-0 md:top-0 top-1/3"
+                >culture</span
+            >
+            <span
+                class="uppercase lg:text-2xl md:text-2xl text-lg absolute -rotate-[5deg] lg:top-1/2 md:top-1/2 top-2/3 lg:left-6 md:left-6"
+                >club</span
+            >
+
+            日本<br class="lg:hidden md:hidden" />文化<br />クラブ
+        </div>
+    </div>
+
+    <div
+        class="hidden rounded-r rounded-bl-xl bg-[url(/texture.jpg)] bg-theme-main w-[32vw] p-4 fixed right-72 font-notoSerifJP font-bold text-xl top-1/2"
+    >
+        2024 年 10 月 18 日 (金) 午後 5 時から午後 8
+        時まで開催される毎年恒例のアイスブレイク イベントにぜひご参加ください。
+        <br /> <br />
+        Join us in our annual ice breaking event on October 18, 2024 (Friday) 5:00pm
+        to 8:00pm.
     </div>
 
     <div class="fixed top-12 right-12 z-10">
@@ -116,6 +141,11 @@
 
     .outer-spin {
         animation: outer-spin 30s linear infinite;
+    }
+
+    .heading-text {
+        /* border: 2px solid #FCF1FD; */
+        /* -webkit-text-stroke: 2px #FCF1FD; */
     }
 
     @keyframes spin {
